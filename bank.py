e66aaa22df80c6292d0f3f2e0e3b644263257361
Henry200714 <h6972308@gmail.com>
2025-06-17 16:37:50 +0100

    Created the Transaction class for the Bank App

--- bank.py
+++ bank.py
@@ -1,4 +1,15 @@
 import streamlit as st
 from datetime import datetime
 
+class Transaction:
+    def __init__(self, amount, type, timestamp=None):
+        self.amount = amount
+        self.type = type
+        self.timestamp = timestamp or datetime.now()
+
+
+    def __str__(self):
+        return f"{self.timestamp.strftime('%Y-%m-%d %H:%M:%S')} - {self.type}: ₦{self.amount}"
+
+
 
